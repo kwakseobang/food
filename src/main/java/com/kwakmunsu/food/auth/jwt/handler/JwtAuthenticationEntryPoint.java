950321cf2d40d05eac2d26267d8062c7150f96e4
kwakseobang <iii6602@naver.com>
2025-01-07 18:28:36 +0900

    feat: jwt 관련 예외처리

---
+++ src/main/java/com/kwakmunsu/food/auth/jwt/handler/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,26 @@
+package com.kwakmunsu.food.auth.jwt.handler;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+// 401 에러 --> 토큰 인증 에러 시 여기서 에러를 잡는다.
+@Log4j2
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+        // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+
+}
+
