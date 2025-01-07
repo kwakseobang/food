119035ad2b58022bd8f0db48cd87190a4b3fee6e
kwakseobang <iii6602@naver.com>
2025-01-07 18:24:33 +0900

    feat: jwt 관련 설정

--- src/main/java/com/kwakmunsu/food/global/config/SecurityConfig.java
+++ src/main/java/com/kwakmunsu/food/global/config/SecurityConfig.java
@@ -1,6 +1,11 @@
 package com.kwakmunsu.food.global.config;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kwakmunsu.food.auth.jwt.JwtProvider;
+import com.kwakmunsu.food.auth.jwt.filter.JwtFilter;
+import com.kwakmunsu.food.auth.jwt.handler.JwtAccessDeniedHandler;
+import com.kwakmunsu.food.auth.jwt.handler.JwtAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +14,21 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity // 시큐리티에서 관리하게 됨
 public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
     private final String[] adminUrl = {"/admin/**"};
-    private final String[] permitAllUrl = {"/","/error","/auth/**","/swagger/**","/swagger-ui/**","/v3/api-docs/**"};
+    private final String[] permitAllUrl = {
+            "/","/error","/auth/**","/members/username/**","/members/nickname/**",
+            "/swagger/**","/swagger-ui/**","/v3/api-docs/**"
+    };
     private final String[] hasRoleUrl = {"/food/**"};
 
     @Bean
@@ -43,7 +56,14 @@ public class SecurityConfig {
                                 .anyRequest().authenticated());// 위에서 처리 못한 나머지 경로를 로그인한 사용자만 접근 허용.
                         // 상단부터 check하게 됨.
 
-
+        // 예외 처리
+        http
+                .exceptionHandling(handle -> handle
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler));
+        // 필터 적용
+        http
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
