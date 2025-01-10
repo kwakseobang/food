eb89537630345968c105668fdeb0d8364e823739
kwakseobang <iii6602@naver.com>
2025-01-10 15:35:23 +0900

    feat: saveBuilder 추가

--- src/main/java/com/kwakmunsu/food/auth/jwt/domain/RefreshToken.java
+++ src/main/java/com/kwakmunsu/food/auth/jwt/domain/RefreshToken.java
@@ -4,11 +4,11 @@ package com.kwakmunsu.food.auth.jwt.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class RefreshToken {
@@ -17,4 +17,13 @@ public class RefreshToken {
     private String token;
 
     private Long memberId;
+
+
+
+
+    @Builder(builderClassName = "RefreshTokenSaveBuilder", builderMethodName = "RefreshTokenSaveBuilder")
+    public RefreshToken(String token,Long memberId) {
+        this.token = token;
+        this.memberId = memberId;
+    }
 }
