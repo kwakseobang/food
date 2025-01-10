a517810fc7683315ea6f63fe5c408df572f59678
kwakseobang <iii6602@naver.com>
2025-01-10 15:42:59 +0900

    feat: at 재발급

--- src/main/java/com/kwakmunsu/food/auth/presentation/LoginController.java
+++ src/main/java/com/kwakmunsu/food/auth/presentation/LoginController.java
@@ -12,11 +12,12 @@ import com.kwakmunsu.food.global.response.FoodResponseCode;
 import com.kwakmunsu.food.global.response.ResponseData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +31,7 @@ public class LoginController {
 
     private final LoginService loginService;
     @PostMapping("/signup") // 중복 API는 따로 만듦. 해당 경로를 클라이언트에서 요청 시 유효하기 때문에 바로 DB 저장
-    @Operation(summary = "회원가입 [JWT X]")
+    @Operation(summary = "회원가입 ")
     public ResponseEntity<ResponseData> signup(
             @RequestBody AuthRequestDto.SignUpRequest signUpRequestDto
     ) {
@@ -39,23 +40,32 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    @Operation(summary = "로그인 [JWT X]")
+    @Operation(summary = "로그인 ")
     public ResponseEntity<ResponseData<TokenResponseDto.AccessTokenResponse>> login(
             @RequestBody AuthRequestDto.LoginRequest loginRequest,
             HttpServletResponse response
     ) {
-        MemberTokens memberTokens = loginService.login(loginRequest);
-        final ResponseCookie cookie = ResponseCookie.from("refresh", memberTokens.getRefreshToken())
-                .maxAge(14 * 24 * 60 * 60)
-                .sameSite("None")
-//                .secure(true)
-                .httpOnly(true)
-                .path("/")
-                .build();
-        response.addHeader(SET_COOKIE, cookie.toString());
+        MemberTokens memberTokens = loginService.login(loginRequest,response);
+
         return  ResponseData.toResponseEntity(
                 FoodResponseCode.LOGIN_SUCCESS,
                 new AccessTokenResponse(memberTokens.getAccessToken())
         );
     }
+
+
+    @PostMapping("/reissue")
+    @Operation(summary = "로그인 유지 - JWT Access Token 재발급")
+    public ResponseEntity<ResponseData<TokenResponseDto.AccessTokenResponse>> reissue(
+            @CookieValue("refresh") final String refreshTokenRequest,
+            HttpServletResponse response
+    ) {
+        MemberTokens memberTokens = loginService.reissue(refreshTokenRequest,response);
+
+        return ResponseData.toResponseEntity(
+                FoodResponseCode.REISSUE_SUCCESS,
+                new AccessTokenResponse(memberTokens.getAccessToken()));
+    }
+
+
 }
--- src/main/java/com/kwakmunsu/food/auth/service/LoginService.java
+++ src/main/java/com/kwakmunsu/food/auth/service/LoginService.java
@@ -1,27 +1,31 @@
 package com.kwakmunsu.food.auth.service;
 
 
+import static org.springframework.http.HttpHeaders.SET_COOKIE;
+
 import com.kwakmunsu.food.auth.dto.request.AuthRequestDto;
-import com.kwakmunsu.food.auth.dto.response.TokenResponseDto;
 import com.kwakmunsu.food.auth.jwt.JwtProvider;
 import com.kwakmunsu.food.auth.jwt.domain.MemberTokens;
 import com.kwakmunsu.food.auth.jwt.domain.RefreshToken;
 import com.kwakmunsu.food.auth.jwt.repository.RefreshTokenRepository;
-import com.kwakmunsu.food.global.exception.FoodException;
+import com.kwakmunsu.food.global.exception.FoodBadRequestException;
 import com.kwakmunsu.food.global.exception.FoodNotFoundException;
 import com.kwakmunsu.food.global.response.FoodErrorCode;
 import com.kwakmunsu.food.member.domain.Member;
 import com.kwakmunsu.food.member.repositroy.MemberRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class LoginService {
+
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final MemberRepository memberRepository;
     private final RefreshTokenRepository refreshTokenRepository;
@@ -34,15 +38,68 @@ public class LoginService {
         memberRepository.save(signUpRequest.toMember(encodedPassword));
     }
 
+    //  UserDetailsService, UserDetails을 필수적으로 구현해야돼ㅣ는 것은 아니다. 이번엔 그냥 loginservice에서 사용자 정보 조회함.
     @Transactional
     public MemberTokens login(
-            AuthRequestDto.LoginRequest loginRequest
-            ) {
+            AuthRequestDto.LoginRequest loginRequest,
+            HttpServletResponse response
+    ) {
         Member member = memberRepository.findByUsername(loginRequest.getUsername())
                 .orElseThrow(() -> new FoodNotFoundException(FoodErrorCode.NOT_FOUND_USER));
+        // mathes(평문 패스워드, 암호화 패스워드) 순서로 해야 됨.
+        if (!bCryptPasswordEncoder.matches(loginRequest.getPassword(), member.getPassword())) {
+            throw new FoodBadRequestException(FoodErrorCode.BAD_REQUEST_PASSWORD);
+        }
+        return getMemberTokens(response, member);
+    }
 
-        MemberTokens memberTokens = jwtProvider.createTokens(member.getId());
-        RefreshToken refreshToken = new RefreshToken( memberTokens.getRefreshToken(), member.getId());
+
+
+    @Transactional
+    public MemberTokens reissue(
+            String refreshTokenRequest,
+            HttpServletResponse response
+    ) {
+        String memberId = jwtProvider.getSubject(refreshTokenRequest);
+        Member member = memberRepository.findById(Long.parseLong(memberId))
+                .orElseThrow(() -> new FoodNotFoundException(FoodErrorCode.NOT_FOUND_USER));
+        log.info("요청 토큰: " + refreshTokenRequest);
+
+        if( !refreshTokenRepository.existsById(refreshTokenRequest) ) {
+                throw  new FoodBadRequestException(FoodErrorCode.BAD_REQUEST_TOKEN);
+        }
+        refreshTokenRepository.deleteById(refreshTokenRequest); // refreshtoken 일회용으로 사용하기 때문에 삭제
+        return getMemberTokens(response, member);
+    }
+
+
+
+    // =================== 유틸성 메소드 ===================
+    private RefreshToken createRefreshToken(String token, long memberId) {
+        return RefreshToken.RefreshTokenSaveBuilder()
+                .token(token)
+                .memberId(memberId)
+                .build();
+    }
+
+    private ResponseCookie crateCookie(final String refreshToken) {
+        return  ResponseCookie.from("refresh",refreshToken)
+                .maxAge(14 * 24 * 60 * 60)
+                .sameSite("None")
+//                .secure(true)
+                .httpOnly(true)
+                .path("/")
+                .build();
+    }
+
+    private MemberTokens getMemberTokens(HttpServletResponse response, Member member) {
+        MemberTokens memberTokens = jwtProvider.createTokens(member);
+        RefreshToken refreshToken = createRefreshToken(
+                memberTokens.getRefreshToken(),
+                member.getId()
+        );
+        ResponseCookie responseCookie = crateCookie(refreshToken.getToken());
+        response.addHeader(SET_COOKIE, responseCookie.toString());
         refreshTokenRepository.save(refreshToken);
         return memberTokens;
     }
