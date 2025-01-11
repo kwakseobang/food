b26bbce904d893ca868e17c5bc96184f65a01a26
kwakseobang <iii6602@naver.com>
2025-01-11 14:47:15 +0900

    feat: 회원 정보 수정 API

--- src/main/java/com/kwakmunsu/food/member/presentation/MemberController.java
+++ src/main/java/com/kwakmunsu/food/member/presentation/MemberController.java
@@ -1,11 +1,9 @@
 package com.kwakmunsu.food.member.presentation;
 
 
-import com.kwakmunsu.food.global.exception.FoodException;
-import com.kwakmunsu.food.global.response.ErrorResponse;
-import com.kwakmunsu.food.global.response.FoodErrorCode;
 import com.kwakmunsu.food.global.response.FoodResponseCode;
 import com.kwakmunsu.food.global.response.ResponseData;
+import com.kwakmunsu.food.member.dto.request.MemberRequestDto;
 import com.kwakmunsu.food.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -13,8 +11,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @Tag(name = "Member")
@@ -26,17 +27,27 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/nickname/{nickname}")
-    @Operation(summary = "닉네임 중복 확인 [JWT X]")
+    @Operation(summary = "닉네임 중복 확인 ")
     public ResponseEntity<ResponseData> isExistNickname(@PathVariable String nickname) {
         memberService.isExistNickname(nickname);
         return ResponseData.toResponseEntity(FoodResponseCode.NICKNAME_SUCCESS);
 
     }
     @GetMapping("/username/{username}")
-    @Operation(summary = "ID 중복 확인 [JWT X]")
+    @Operation(summary = "ID 중복 확인 ")
     public ResponseEntity<ResponseData> isExistUsername(@PathVariable String username) {
         memberService.isExistUsername(username);
         return ResponseData.toResponseEntity(FoodResponseCode.USERNAME_SUCCESS);
 
+    }
+
+    @PutMapping()
+    @Operation(summary = "회원 정보 변경")
+    public ResponseEntity<ResponseData> update(
+            @RequestPart(required = false) MultipartFile image,
+            @RequestPart(value = "updateInfo") MemberRequestDto.UpdateInfoRequest updateRequest
+    ) {
+        memberService.updateMember(image, updateRequest.getNickname());
+        return ResponseData.toResponseEntity(FoodResponseCode.UPDATE_MEMBER);
     }
 }
--- src/main/java/com/kwakmunsu/food/member/service/MemberService.java
+++ src/main/java/com/kwakmunsu/food/member/service/MemberService.java
@@ -2,16 +2,22 @@ package com.kwakmunsu.food.member.service;
 
 
 import com.kwakmunsu.food.global.exception.FoodBadRequestException;
+import com.kwakmunsu.food.global.exception.FoodNotFoundException;
+import com.kwakmunsu.food.global.image.service.S3ImageService;
 import com.kwakmunsu.food.global.response.FoodErrorCode;
+import com.kwakmunsu.food.global.util.JwtUtil;
+import com.kwakmunsu.food.member.domain.Member;
 import com.kwakmunsu.food.member.repositroy.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final S3ImageService s3ImageService;
 
     @Transactional(readOnly = true)
     public void isExistUsername(String username) {
@@ -25,5 +31,43 @@ public class MemberService {
         if (memberRepository.existsByNickname(nickname))  {
             throw new FoodBadRequestException(FoodErrorCode.DUPLICATE_NICKNAME);
         }
+    }
+    @Transactional
+    public void updateMember(MultipartFile image, String nickname) {
+        // ===========  프로필 변경 ====================
+        // 1. 기본 프로필 -> 원하는 사진 - currentImage = x , image = O
+        // 2. 기존 사진 -> 기본 프로필  - currentImage = o , image = x
+        // 3. 기존 사진 -> 새로운 사진 - currentImage = o , image = O
+
+        Member member = findCurrentMember();
+        // 프론트에서 기존 닉네임과 같다면 필터링할것이지만 2차적으로 검사. 기존 닉네임과 같을 경우는 제외하고 검사.
+        if (!member.getNickname().equals(nickname)) {
+            isExistNickname(nickname);  // 닉네임 중복 검사.
+        }
+        String currentImage = member.getImageUrl();
+
+        if (currentImage != null) { // 2,3 번
+            s3ImageService.deleteImage(currentImage);
+        }
+        // S3 업로드 경우의 수.
+        String newImage = image != null ?  s3ImageService.uploadImage(image) : currentImage;
+        // 트랜잭션의 영속성 컨텍스트 특징의 따라 엔티티의 변경 감지 시 DB 자동 Update, 값이 변경되지 않았을 경우 Update x
+        member.updateImage(newImage);
+        member.updateNickname(nickname);
+
+    }
+
+
+    @Transactional(readOnly = true)
+     public Member findCurrentMember() {
+        Long currentMemberId = JwtUtil.getCurrentMemberId();
+        Member currentMember = getCurrentUser(currentMemberId);
+        return currentMember;
+    }
+
+    @Transactional(readOnly = true)
+    public Member getCurrentUser(Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(
+                () -> new FoodNotFoundException(FoodErrorCode.NOT_FOUND_USER));
     }
 }
